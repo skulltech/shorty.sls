6405f5ef7ece73dcf4d6c38a6fcab7e663bd6952
Sumit Ghosh <sumit@sumit-ghosh.com>
2020-06-28 13:20:09 +0530

    Functional url shortener

--- app.py
+++ app.py
@@ -1,54 +1,70 @@
-from flask import Flask, jsonify, request, redirect, abort
+import json
 import os
-import boto3
-import string
 import random
 import re
+import string
+from decimal import Decimal
+
+import boto3
+from flask import Flask, jsonify, request, redirect, abort
 
 app = Flask(__name__)
 URLS_TABLE = os.environ['URLS_TABLE']
 IS_OFFLINE = os.environ.get('IS_OFFLINE')
 
 if IS_OFFLINE:
-    client = boto3.client(
+    db = boto3.resource(
         'dynamodb',
         region_name='localhost',
         endpoint_url='http://localhost:8000'
-        )
+    )
 else:
-    client = boto3.client('dynamodb')
+    db = boto3.resource('dynamodb')
+table = db.Table(URLS_TABLE)
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+app.json_encoder = DecimalEncoder
 
 
 @app.route('/<string:url_id>')
 def shorty(url_id):
-    response = client.get_item(
-        TableName=URLS_TABLE, 
-        Key={
-            'urlId': {'S': url_id}
-        })
+    inspect = False
+    if url_id[-1] == '+':
+        url_id = url_id[:-1]
+        inspect = True
+
+    response = table.get_item(Key={'urlId': url_id})
     item = response.get('Item')
     if not item:
         abort(404)
+    long_url = item.get('longUrl')
 
-    long_url = item.get('longUrl').get('S')
+    if inspect:
+        return long_url
+
+    response = table.update_item(
+        Key={'urlId': url_id},
+        UpdateExpression='ADD visits :inc',
+        ExpressionAttributeValues={':inc': 1}
+    )
     return redirect(long_url, code=301)
 
 
 @app.route('/api/urls/<string:url_id>')
 def get_url(url_id):
-    response = client.get_item(
-        TableName=URLS_TABLE, 
-        Key={
-            'urlId': {'S': url_id}
-        })
+    response = table.get_item(Key={'urlId': url_id})
     item = response.get('Item')
     if not item:
         return jsonify({'error': 'URL does not exist'}), 404
-    
-    return jsonify({
-        'urlId': item.get('urlId').get('S'),
-        'longUrl': item.get('longUrl').get('S')
-        })
+
+    return jsonify(item)
 
 
 @app.route('/api/urls', methods=['POST'])
@@ -62,15 +78,19 @@ def create_url():
     elif not re.match('^[a-z0-9._-]{1,16}$', url_id):
         return jsonify({'error': 'Invalid urlId, must contain only lowercase letters, hyphen, dot and underscore'}), 400
 
-    response = client.put_item(
-        TableName=URLS_TABLE,
-        ConditionExpression='attribute_not_exists(url_id)',
-        Item={
-            'urlId': {'S': url_id},
-            'longUrl': {'S': long_url}
-        })
+    try:
+        response = table.put_item(
+            ConditionExpression='attribute_not_exists(urlId)',
+            Item={
+                'urlId': url_id,
+                'longUrl': long_url,
+                'visits': 0
+            })
+    except db.meta.client.exceptions.ConditionalCheckFailedException as e:
+        return jsonify({'error': 'urlId already exists'}), 400
 
     return jsonify({
         'urlId': url_id,
-        'longUrl': long_url
-        })
+        'longUrl': long_url,
+        'visits': 0
+    })
